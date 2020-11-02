39fbecbf53e89abe51f396973a152a317297a48f
Tushar Saxena <stushar345@gmail.com>
2020-11-02 22:21:02 +0530

    Add files via upload

---
+++ two.cpp
@@ -0,0 +1,23 @@
+void abc(vector<int> g[],int i,int visited[],vector<int> &a)
+{
+    visited[i]=1;
+    a.push_back(i);
+    for(int x: g[i])
+    {
+    if(visited[x]==-1)
+        abc(g,x,visited,a);
+    }
+}
+vector <int> dfs(vector<int> g[], int N)
+{
+    
+   int visited[N];
+   for(int i=0;i<N;i++)
+   {
+       visited[i]=-1;
+   }
+   
+   vector<int> a;
+   abc(g,0,visited,a);
+return a;
+}
